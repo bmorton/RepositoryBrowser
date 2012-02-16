e632658b1ccaa267117c2990613b58c5e4712c77
Brian Morton <bmorton@sdreader.com>
2012-02-16 00:32:13 -0800

    Initial commit.

---
+++ Code/Other/RBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RBAppDelegate.h
+//  RepositoryBrowser
+//
+//  Created by Brian Morton on 2/16/12.
+//  Copyright (c) 2012 RestKit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Code/Other/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  RepositoryBrowser
+//
+//  Created by Brian Morton on 2/16/12.
+//  Copyright (c) 2012 RestKit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RBAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RBAppDelegate class]));
+    }
+}
